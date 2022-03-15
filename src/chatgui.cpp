50e1b71540b8105d64f8adc0071e1694a654dbf5
felipefernandezayala <felipe.fernandez.ayala@gmail.com>
2022-03-15 01:51:41 +0000

    task 1 done

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic.reset( new ChatLogic() ); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +135,7 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // no need to delete
 
     ////
     //// EOF STUDENT CODE
